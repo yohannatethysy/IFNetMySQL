3796bd95666a5fee2de678f5e8464436476867c4
Maria Clara Ferreira Rodrigues <mariaclarafr01@gmail.com>
2017-12-06 17:50:53 -0300

    Update Usuario.py

--- IFNetMySQL/model/Usuario.py
+++ IFNetMySQL/model/Usuario.py
@@ -2,25 +2,44 @@ import mysql.connector
 from database.configDB import config
 
 class Usuario():
-    def __init__(self, nome, email):
+    def __init__(self, nome, email, senha):
 
         self.nome = nome
         self.email = email
-
-    def inserirUsuario(self, nome, email):
+        self.senha = senha
+        
+    def inserirUsuario(self, nome, email, senha):
         conn = mysql.connector.connect(**config)
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO TB_Usuario VALUES(%s,%s)", (nome, email))
+        cursor.execute("INSERT INTO TB_Usuario VALUES(?,?, ?)", (nome, email, senha))
         conn.commit()
         conn.close()
 
-    def listarUsuario(self):
+    def listarUsuarios(self):
         conn = mysql.connector.connect(**config)
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM TB_Usuario")
+        cursor.execute("SELECT nome FROM TB_Usuario")
 
         for linha in cursor.fetchall():
             print(linha)
 
         conn.commit()
         conn.close()
+        
+    def  deletarUsuario ( self, email, senha):
+        conn = mysql.connector.connect(**config)
+        cursor = conn.cursor() 
+        senhaC = self.senha
+        se (senha ==  senhaC):
+            cursor.execute ( "" "
+                DELETE FROM tb_usuario 
+                ONDE email =?
+                "" " , (email))
+         conn.commit ()
+         conn.close ()
+                            
+    def  __str__ (self):
+        retorno  " Usuario %s"  % (self.nome)
+
+    def  __repr__ (self):
+        retornar a  si mesmo . __str__ ()
